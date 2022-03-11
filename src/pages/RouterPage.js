13432023607530cb42b0bc476f92b19523b352b2
Obinna Ukpabi <askphntm@gmail.com>
2022-03-11 11:53:21 +0100

    updated website pages

--- src/pages/RouterPage.js
+++ src/pages/RouterPage.js
@@ -1,4 +1,4 @@
-import {HashRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import ScrollToTop from './ScrollToTop';
 import Home from './Home';
 import About  from './About';
